3f03f1192f96cd25336f050c822f047cadafc8c1
Gwenvael.L <laskar.gwenvael@gmail.com>
2020-10-01 11:32:51 +0200

    add folder exo

---
+++ index.js
@@ -0,0 +1,11 @@
+const appel = require("./information.js");
+
+console.log(appel);
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const info = {
+name : "gwen",
+campus : "Nantes"};
+
+module.exports = info;
+
